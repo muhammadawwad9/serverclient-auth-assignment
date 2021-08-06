5dd722513c5dccf1dfa42df08277261977ce8056
Muhammad Awwad <hammode.awad@gmail.com>
2021-08-06 03:27:44 +0300

    build command for netlify

---
+++ frontend/build/precache-manifest.9588733c95db17f946067d0afe86bb44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3eaccc9940977f646c519b1146c7672",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "5c1e449865971968158a",
+    "url": "/material-dashboard-react/static/css/2.99e074f4.chunk.css"
+  },
+  {
+    "revision": "623d6c75a6ff988c6613",
+    "url": "/material-dashboard-react/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "5c1e449865971968158a",
+    "url": "/material-dashboard-react/static/js/2.7a88c0d1.chunk.js"
+  },
+  {
+    "revision": "623d6c75a6ff988c6613",
+    "url": "/material-dashboard-react/static/js/main.201df936.chunk.js"
+  },
+  {
+    "revision": "fed1e2592e13fb10ce3d",
+    "url": "/material-dashboard-react/static/js/runtime~main.417f980f.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
